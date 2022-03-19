aa1a6691fce7480f397bbe9d7ae1d8cf894d869f
blue <d33pblue96@gmail.com>
2022-03-19 10:51:00 +0100

    moved instructionresponses to different file

--- mcuConnection/InstructionCodes.cs
+++ mcuConnection/InstructionCodes.cs
@@ -18,21 +18,5 @@ namespace mcuConnection
 
     }
 
-    [Flags]
-    public enum InstructionResponses : byte
-    {
-        Nop,
-        CreatedConnection,
-        ClosedConnection,
-        SendingMemData,
-        HandBrakeUnlocked,
-        HandbrakeLocked,
-        CalibrationStarted,
-        CalibrationStopped,
-        SendingCalibrationResult,
-        SendingTotalCapacity ,
-        ConnectionError,
-        SendingRemainingBatteryCharge,
-        
-    }
+   
 }
